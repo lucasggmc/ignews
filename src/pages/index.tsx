66fdb47d7ab2021c12d8503022e698f5e3b3976b
lucasggmc <lucasggmc@gmail.com>
2021-08-10 22:43:52 -0300

    finalizado integração com a API de pagamentos stripe e renderização SSR e SSG do valor do produto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,79 @@
+import { GetServerSideProps, GetStaticProps } from 'next';
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
-export default function Home() {
+import styles from './home.module.scss';
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
+  console.log('product', product);
   return (
     <>
       <Head>
-        <title>Início | ig.news </title>
+        <title>Home | ig.news </title>
       </Head>
-      <h1>Hello</h1>
+      
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏  Hey, welcome</span>
+          <h1>News about the <span>React</span>world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for { product.amount } month</span>
+          </p>
+          <SubscribeButton priceId={product.priceId}/>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+
+      </main>
+
     </>
   )
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1JN5FxA2NhPGZou8EZU7StSf');
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100)
+  }
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24 //24 hours
+  }
+}
+
+
+////SSR
+// export const getServerSideProps:GetServerSideProps = async () => {
+//   const price = await stripe.prices.retrieve('price_1JN5FxA2NhPGZou8EZU7StSf');
+
+//   const product = {
+//     priceId: price.id,
+//     amount: new Intl.NumberFormat('en-US', {
+//       style: 'currency',
+//       currency: 'USD'
+//     }).format(price.unit_amount / 100)
+//   }
+
+//   return {
+//     props: {
+//       product
+//     }
+//   }
+// }
