c0a16aa789f3510ed2bb2219260c8ec4d085eace
lucasggmc <lucasggmc@gmail.com>
2021-08-14 19:45:50 -0300

    adicionado integração e validação de pagamentos com o Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,7 @@
 
+import { useSession, signIn } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -6,11 +9,35 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps){
+    const [session] = useSession();
+
+
+    async function handleSubscribe(){
+        if(!session){
+            signIn('github');
+            return;
+        }
+
+        try {
+            const response = await api.post('/subscribe')
+            console.log('response', response);
+
+            const { sessionId } = response.data;
+
+            const stripe = await getStripeJs()
+
+            await stripe.redirectToCheckout({ sessionId })
+        } catch (error) {
+            alert(error.message);
+        }
+
+    }
 
     return(
         <button 
             type="button"
             className={styles.subscribeButton}  
+            onClick={handleSubscribe}
         >
             Subscribe now
         </button>
